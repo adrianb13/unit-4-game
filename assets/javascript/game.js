2053cf092b8fd538d84b17163af88a90f81a4c64
Adrian Brinoes <Coding4Adrian@yahoo.com>
2018-12-11 18:22:12 -0800

    index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,7 +16,6 @@ var yourScore = 0;
 var wins = 0;
 var losses = 0;
 
-
 var randomNumber = (Math.floor(Math.random() * 102) + 19);
 console.log(randomNumber);
 $("#randomNumber").text(randomNumber);
@@ -47,6 +46,7 @@ $("#crystal1").on("click", function() {
 
     winOrLose();
     console.log(yourScore);
+    console.log(crystal1);
     }); 
 
 $("#crystal2").on("click", function() {
@@ -58,6 +58,7 @@ $("#crystal2").on("click", function() {
 
     winOrLose();
     console.log(yourScore);
+    console.log(crystal2);
     }); 
 
 $("#crystal3").on("click", function() {
@@ -69,6 +70,7 @@ $("#crystal3").on("click", function() {
 
     winOrLose();
     console.log(yourScore);
+    console.log(crystal3);
     }); 
 
 $("#crystal4").on("click", function() {
@@ -81,7 +83,7 @@ $("#crystal4").on("click", function() {
     winOrLose();
     console.log(yourScore);
     console.log(counter4);
-
+    console.log(crystal4);
     });
 
 function winOrLose () {
@@ -90,35 +92,29 @@ function winOrLose () {
         losses = losses + 1;
         $("#losses").text(losses);
         console.log(losses);
-        resetAll()
-        yourScore = 0;
-        counter1 = 0;
-        counter2 = 0;
-        counter3 = 0;
-        counter4 = 0;
+        resetAll();
     } else if (yourScore === randomNumber) {
         alert("You Win");
         wins = wins + 1;
         $("#wins").text(wins);
         console.log(wins);
         resetAll();
-        yourScore = 0;
-        counter1 = 0;
-        counter2 = 0;
-        counter3 = 0;
-        counter4 = 0;
       }
 };
 
 function resetAll () {
-    var randomNumber = "";
-    var crystalNumbers = [];
-    var crystal1 = "";
-    var crystal2 = "";
-    var crystal3 = "";
-    var crystal4 = "";
-    var yourScore = 0;
-    var randomNumber = (Math.floor(Math.random() * 102) + 19);
+    randomNumber = "";
+    crystalNumbers = [];
+    crystal1 = "";
+    crystal2 = "";
+    crystal3 = "";
+    crystal4 = "";
+    counter1 = 0;
+    counter2 = 0;
+    counter3 = 0;
+    counter4 = 0;
+    yourScore = 0;
+    randomNumber = (Math.floor(Math.random() * 102) + 19);
     console.log(randomNumber);
     $("#randomNumber").text(randomNumber);
 
@@ -128,13 +124,13 @@ function resetAll () {
     }
     crystalNumbers.push(Math.floor(Math.random() * 5) *2 +1);
 
-    var crystal1 = crystalNumbers[0];
+        crystal1 = crystalNumbers[0];
         crystal1 = parseInt(crystal1);
-    var crystal2 = crystalNumbers[1];
+        crystal2 = crystalNumbers[1];
         crystal2 = parseInt(crystal2);
-    var crystal3 = crystalNumbers[2];
+        crystal3 = crystalNumbers[2];
         crystal3 = parseInt(crystal3);
-    var crystal4 = crystalNumbers[3];
+        crystal4 = crystalNumbers[3];
         crystal4 = parseInt(crystal4);
     $("#totalScore").text(yourScore);
     console.log(yourScore)
